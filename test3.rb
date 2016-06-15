b626eb4538e6a8375325095ac22b3a27da441cc6
Christopher Gradwohl <christophergradwohl@Christophers-MacBook-Pro-2.local>
2016-06-15 16:06:01 -0700

    boolean stuff

---
+++ test3.rb
@@ -0,0 +1,14 @@
+=begin
+print "Enter a digit: "
+mydigit = gets.chomp
+puts "your entered " + mydigit
+=end
+
+
+
+
+not_hungry = false
+puts " hey i am hungry " unless not_hungry
+
+hungry = true
+puts " hey i am hungry" unless hungry
